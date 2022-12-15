c015d8cb6cad925fb94c4413e7c53d4255e87ca3
Firdous Ali <firdous.ali@innovativesolutions.ae>
2022-12-15 18:55:41 +0500

    h

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/cartSlice';
 import modalReducer from '../features/modal/modalSlice';
+import cakeReducer from '../features/cake/cakeSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     modal: modalReducer,
+    cake: cakeReducer,
   },
 });
