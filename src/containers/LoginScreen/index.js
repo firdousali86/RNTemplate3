a0b51c320a3cc58276e2c5b660db28871633ca9b
Firdous Ali <firdous.ali.tp@gmail.com>
2023-03-28 14:15:27 +0500

    c

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -1,28 +1,43 @@
 // @flow
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
 import PropTypes from 'prop-types';
 import {View, TextInput, TouchableOpacity, Text} from 'react-native';
+import {authActions} from '../../features/auth/authSlice';
 
 import styles from './styles';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
-        onChangeText={() => {}}
-        value={''}
+        onChangeText={text => {
+          setEmail(text);
+        }}
+        value={email}
         placeholder="Email/Username"
         keyboardType="email-address"
       />
       <TextInput
         style={styles.input}
-        onChangeText={() => {}}
-        value={''}
+        onChangeText={text => {
+          setPassword(text);
+        }}
+        value={password}
         placeholder="Password"
         secureTextEntry
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          dispatch(authActions.onLogin({email: email}));
+        }}>
         <Text style={{color: 'white'}}>Login</Text>
       </TouchableOpacity>
     </View>
