40252a340b789c2ade172a3ded6ce60bda663f2e
Firdous Ali <firdous.ali.tp@gmail.com>
2023-04-06 21:16:25 +0500

    saga updated

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
-import {fork} from 'redux-saga/effects';
+import {all} from 'redux-saga/effects';
 import todos from './todos';
 
-export default function* root() {
-  yield fork(todos);
+export default function* rootSaga() {
+  yield all([todos()]);
 }
