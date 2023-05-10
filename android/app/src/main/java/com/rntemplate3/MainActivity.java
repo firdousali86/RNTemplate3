357fa25f3b4b476e983b972d2090f6b665821b04
Firdous Ali <firdous.ali.tp@gmail.com>
2023-05-10 16:40:23 +0500

    working

--- android/app/src/main/java/com/rntemplate3/MainActivity.java
+++ android/app/src/main/java/com/rntemplate3/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnapp;
+package com.rntemplate3;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNApp";
+    return "RNTemplate3";
   }
 
   /**
--- ios/RNTemplate3Tests/RNTemplate3Tests.m
+++ ios/RNTemplate3Tests/RNTemplate3Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNAppTests : XCTestCase
+@interface RNTemplate3Tests : XCTestCase
 
 @end
 
-@implementation RNAppTests
+@implementation RNTemplate3Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
