75f91b6a016b98c1d83cb3ea753058d8be345966
Firdous Ali <firdous.ali.tp@gmail.com>
2023-05-26 14:06:36 +0500

    scrollview ref

--- src/containers/TestUseRef/index.js
+++ src/containers/TestUseRef/index.js
@@ -1,5 +1,11 @@
 import React, {useState, useRef} from 'react';
-import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 const TestUseRef = () => {
   const [field1, setField1] = useState('');
@@ -9,6 +15,7 @@ const TestUseRef = () => {
   const field1Ref = useRef(null);
   const field2Ref = useRef(null);
   const field3Ref = useRef(null);
+  const scrollRef = useRef(null);
 
   const style = {
     margin: 10,
@@ -17,66 +24,85 @@ const TestUseRef = () => {
     color: 'white',
   };
 
+  const renderForm = () => {
+    return (
+      <>
+        <TextInput
+          ref={field1Ref}
+          value={field1}
+          onChangeText={changedText => {
+            setField1(changedText);
+          }}
+          placeholder="Field 1"
+          style={style}
+        />
+        <TextInput
+          ref={field2Ref}
+          value={field2}
+          onChangeText={changedText => {
+            setField2(changedText);
+          }}
+          placeholder="Field 2"
+          style={style}
+        />
+        <TextInput
+          ref={field3Ref}
+          value={field3}
+          onChangeText={changedText => {
+            setField3(changedText);
+          }}
+          placeholder="Field 3"
+          style={style}
+        />
+
+        <TouchableOpacity
+          style={[style, {backgroundColor: 'pink'}]}
+          onPress={() => {
+            field1Ref.current.clear();
+            field2Ref.current.clear();
+            field3Ref.current.clear();
+          }}>
+          <Text>Clear all</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[style, {backgroundColor: 'pink'}]}
+          onPress={() => {
+            field1Ref.current.focus();
+          }}>
+          <Text>Focus on 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[style, {backgroundColor: 'pink'}]}
+          onPress={() => {
+            field2Ref.current.focus();
+          }}>
+          <Text>Focus on 2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[style, {backgroundColor: 'pink'}]}
+          onPress={() => {
+            field3Ref.current.focus();
+          }}>
+          <Text>Focus on 3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[style, {backgroundColor: 'pink'}]}
+          onPress={() => {
+            scrollRef.current.scrollToEnd();
+          }}>
+          <Text>Scroll to end</Text>
+        </TouchableOpacity>
+      </>
+    );
+  };
+
   return (
     <View style={{flex: 1}}>
-      <TextInput
-        ref={field1Ref}
-        value={field1}
-        onChangeText={changedText => {
-          setField1(changedText);
-        }}
-        placeholder="Field 1"
-        style={style}
-      />
-      <TextInput
-        ref={field2Ref}
-        value={field2}
-        onChangeText={changedText => {
-          setField2(changedText);
-        }}
-        placeholder="Field 2"
-        style={style}
-      />
-      <TextInput
-        ref={field3Ref}
-        value={field3}
-        onChangeText={changedText => {
-          setField3(changedText);
-        }}
-        placeholder="Field 3"
-        style={style}
-      />
-
-      <TouchableOpacity
-        style={[style, {backgroundColor: 'pink'}]}
-        onPress={() => {
-          field1Ref.current.clear();
-          field2Ref.current.clear();
-          field3Ref.current.clear();
-        }}>
-        <Text>Clear all</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[style, {backgroundColor: 'pink'}]}
-        onPress={() => {
-          field1Ref.current.focus();
-        }}>
-        <Text>Focus on 1</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[style, {backgroundColor: 'pink'}]}
-        onPress={() => {
-          field2Ref.current.focus();
-        }}>
-        <Text>Focus on 2</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[style, {backgroundColor: 'pink'}]}
-        onPress={() => {
-          field3Ref.current.focus();
-        }}>
-        <Text>Focus on 3</Text>
-      </TouchableOpacity>
+      <ScrollView ref={scrollRef}>
+        {renderForm()}
+        {renderForm()}
+        {renderForm()}
+      </ScrollView>
     </View>
   );
 };
